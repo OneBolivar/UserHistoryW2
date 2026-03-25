6bfe3c659368dc22625c9faeba84627cdb6e172b
One Bolivar <jebolivar33@gmail.com>
2026-03-25 11:57:42 -0500

    OPCION 3 DE SEMANA 3 (FINALIZADA)

--- Option4.py
+++
@@ -1,2 +0,0 @@
-def Option4 (Inventory, product_name):
-    return Inventory.get(product_name, {})
--- menu.py
+++ menu.py
@@ -1,7 +1,9 @@
 from Option1 import OptionNumber1 # Importa la función OptionNumber1 del módulo Option1
 from Option2 import inventory_print # Importa la función para imprimir el inventario de Option2
 import Option2 # Importa el módulo Option2 completo para acceder a sus variables
-from Option3 import calculate_statistics # Importa la función de estadísticas del módulo Option3
+from Option3 import product_search # Importa la función de búsqueda del módulo Option4
+from Option6 import calculate_statistics # Importa la función de estadísticas del módulo Option3
+
 
 def MenuOptions(): 
     VALIDATOR = True 
@@ -11,8 +13,11 @@ def MenuOptions():
         print("=" * 50) 
         print("1. Add product") 
         print("2. Show inventory") 
-        print("3. Calculates statistics") 
-        print("4. Exit") 
+        print("3. Search product") 
+        print("4. Update product") 
+        print("5. Delete product") 
+        print("6. Calculates statistics") 
+        print("7. Exit") 
         
         #---------------------------------------------------------
         VALIDATOR_OPTIONS = True # Variable para validar la entrada del usuario
@@ -27,7 +32,12 @@ def MenuOptions():
                 elif Options == 2: 
                     inventory_print() # Llama a la función que muestra los productos en pantalla
                 elif Options == 3:
-                    print("Aqui va la opcion buscar producto")
+                    product_name = input("Enter the name of the product you wish to search for: ")
+                    resultado = product_search(product_name)
+                    if resultado != {}:
+                        print(f"Product Found: {resultado}")
+                    else:
+                        print("El producto no existe.")
                 elif Options == 4:
                     print("Aqui va la opcion actualizar producto")                
                 elif Options == 5:
